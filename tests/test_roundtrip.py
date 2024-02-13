4197a06ce3592fb27bfb93f5178670b9abd76a16
AJ Friend <ajfriend@gmail.com>
2024-02-13 10:03:50 -0800

    clean up data

--- tests/test_roundtrip.py
+++ tests/test_roundtrip.py
@@ -3,58 +3,48 @@ import seaborn as sns
 import tempfile
 import pytest
 
-def test_filename_input():
+
+def get_dfs():
 	keys = [
 	    'diamonds',
 	    'car_crashes',
 	]
 
-	dfict = {
+	dfs = {
 	    k: sns.load_dataset(k)
 	    for k in keys
 	}
 
-	with tempfile.TemporaryFile() as f:
-		keepdb.to_from.standard.dfs_to_zip(f, dfict)
-		dfict2 = keepdb.to_from.standard.zip_to_dfs(f, use_arrow_dtypes=False)
+	return dfs
 
-	keepdb.are_df_dicts_equal(dfict, dfict2)
+
+def test_filename_input():
+	dfs = get_dfs()
+
+	with tempfile.TemporaryFile() as f:
+		keepdb.to_from.standard.dfs_to_zip(f, dfs)
+		dfs2 = keepdb.to_from.standard.zip_to_dfs(f, use_arrow_dtypes=False)
+
+	keepdb.are_df_dicts_equal(dfs, dfs2)
 
 
 def test_file_object_input():
-	keys = [
-	    'diamonds',
-	    'car_crashes',
-	]
-
-	dfict = {
-	    k: sns.load_dataset(k)
-	    for k in keys
-	}
+	dfs = get_dfs()
 
 	with tempfile.NamedTemporaryFile() as f:
-		keepdb.to_from.standard.dfs_to_zip(f.name, dfict)
-		dfict2 = keepdb.to_from.standard.zip_to_dfs(f.name)
+		keepdb.to_from.standard.dfs_to_zip(f.name, dfs)
+		dfs2 = keepdb.to_from.standard.zip_to_dfs(f.name)
 
-	keepdb.are_df_dicts_equal(dfict, dfict2)
-
+	keepdb.are_df_dicts_equal(dfs, dfs2)
 
 
 def test_filename_input_arrow():
-	keys = [
-	    'diamonds',
-	    'car_crashes',
-	]
-
-	dfict = {
-	    k: sns.load_dataset(k)
-	    for k in keys
-	}
+	dfs = get_dfs()
 
 	with tempfile.TemporaryFile() as f:
-		keepdb.to_from.standard.dfs_to_zip(f, dfict)
-		dfict2 = keepdb.to_from.standard.zip_to_dfs(f, use_arrow_dtypes=True)
+		keepdb.to_from.standard.dfs_to_zip(f, dfs)
+		dfs2 = keepdb.to_from.standard.zip_to_dfs(f, use_arrow_dtypes=True)
 
 	with pytest.raises(KeyError, match='DictionaryType'):
 		# this should work but currently doesn't. i think i'm waiting on https://github.com/pandas-dev/pandas/issues/57395
-		keepdb.are_df_dicts_equal(dfict, dfict2)
+		keepdb.are_df_dicts_equal(dfs, dfs2)
