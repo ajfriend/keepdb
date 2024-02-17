bb4cc5373c1be503d66218262e5c6ce304161e11
ajfriend <ajfriend@gmail.com>
2024-02-17 10:05:52 -0800

    nothing much

--- src/keepdb/_version.py
+++ src/keepdb/_version.py
@@ -1,3 +1,8 @@
 from importlib import metadata
 
 __version__ = metadata.version('keepdb')
+
+"""
+Alternative approaches:
+https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
+"""
